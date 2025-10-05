f6cd4617d0710bcbe0359077c2ed3df76a2c5e3b
Vinicius Virgilli <vinicius.virgilli3@gmail.com>
2025-10-05 10:48:38 -0300

    wip - implementa cache no balde para deletar após 1h de inatividade

--- src/main/java/org/viniciusvirgilli/ratelimit/RateLimitFilter.java
+++ src/main/java/org/viniciusvirgilli/ratelimit/RateLimitFilter.java
@@ -1,8 +1,11 @@
 package org.viniciusvirgilli.ratelimit;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Refill;
+import io.github.bucket4j.VerboseResult;
 import io.quarkus.runtime.Startup;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -11,8 +14,10 @@ import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.sql.Ref;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -20,6 +25,7 @@ import java.time.ZoneId;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @Provider
 @ApplicationScoped
 @Startup
@@ -41,26 +47,44 @@ public class RateLimitFilter implements ContainerRequestFilter, ContainerRespons
     long minutosBloqueio;
 
     // bucket = balde
-    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+    private final Cache<String, Bucket> baldes = Caffeine.newBuilder()
+            .expireAfterAccess(Duration.ofDays(1))
+            .build();
+
     private final Map<String, Long> ipsBloqueados = new ConcurrentHashMap<>();
 
     @Override
     public void filter(ContainerRequestContext requestContext) {
+        log.info(System.lineSeparator());
+        log.debug("[Inicio filtro] ipsBloqueados.size={}, baldes.size={}", ipsBloqueados.size(), baldes.estimatedSize());
+
         if (!enable) {
             return;
         }
 
         String ip = extraiIp(requestContext);
+        log.debug("Requisicao recebida de IP: {}", ip);
+
 
         if (isIpBloqueado(ip)) {
+            log.info("Requisicao abortada: IP {} ainda bloqueado", ip);
             requestContext.abortWith(respostaToManyRequests(ip));
             return;
         }
 
-        Bucket bucket = buckets.computeIfAbsent(ip, this::criaBuckets);
+        Bucket balde = baldes.get(ip, k -> criaBalde());
 
-        if (!bucket.tryConsume(1)) { ipsBloqueados.put(ip, System.currentTimeMillis() + minutosBloqueio*60000);
-            requestContext.abortWith(respostaToManyRequests(ip)); }
+        boolean consumiu = balde.tryConsume(1);
+
+        if (!consumiu) {
+            long instanteDesbloqueio = System.currentTimeMillis() + minutosBloqueio*60000;
+
+            ipsBloqueados.put(ip, instanteDesbloqueio);
+            log.info("Requisicao abortada: IP {} excedeu o limite de requisicoes", ip);
+            requestContext.abortWith(respostaToManyRequests(ip));
+        } else {
+            log.info("[Ratelimit] - IP: {} consumiu 1 token. Tokens restantes (aprox): {}", ip, balde.getAvailableTokens());
+        }
     }
 
     @Override
@@ -68,21 +92,42 @@ public class RateLimitFilter implements ContainerRequestFilter, ContainerRespons
         if (!enable) return;
 
         String ip = extraiIp(requestContext);
-        Bucket bucket = buckets.get(ip);
-        if (bucket != null) {
-            long requestsDisponiveis = bucket.getAvailableTokens();
+        Bucket balde = baldes.get(ip, k -> criaBalde());
 
-            responseContext.getHeaders().add("X-RateLimit-Available", requestsDisponiveis);
-            responseContext.getHeaders().add("X-RateLimit-Limits",
-                    String.format("%d/s, %d/m, %d/h",
-                            requestsPorSegundo, requestsPorMinuto, requestsPorHora));
+        if (balde != null) {
+            populaHeadersResponse(responseContext, balde);
         }
     }
 
-    private Bucket criaBuckets(String ip) {
-        Bandwidth limitePorSegundo = Bandwidth.simple(requestsPorSegundo, Duration.ofSeconds(1));
-        Bandwidth limitePorMinuto = Bandwidth.simple(requestsPorMinuto, Duration.ofMinutes(1));
-        Bandwidth limitePorHora = Bandwidth.simple(requestsPorHora, Duration.ofHours(1));
+    private void populaHeadersResponse(ContainerResponseContext responseContext, Bucket balde) {
+        VerboseResult<Long> verbose = balde.asVerbose().getAvailableTokens();
+        long[] disponiveisPorBanda = verbose.getDiagnostics().getAvailableTokensPerEachBandwidth();
+        Bandwidth[] bandas = verbose.getConfiguration().getBandwidths();
+
+        if (disponiveisPorBanda.length> 0) {
+            responseContext.getHeaders().add("X-RateLimit-available-Second", String.valueOf(disponiveisPorBanda[0]));
+            responseContext.getHeaders().add("X-RateLimit-Limit-Second", String.valueOf(bandas[0].getCapacity()));
+        }
+        if (disponiveisPorBanda.length> 1) {
+            responseContext.getHeaders().add("X-RateLimit-available-Minute", String.valueOf(disponiveisPorBanda[1]));
+            responseContext.getHeaders().add("X-RateLimit-Limit-Minute", String.valueOf(bandas[1].getCapacity()));
+        }
+        if (disponiveisPorBanda.length> 2) {
+            responseContext.getHeaders().add("X-RateLimit-available-Hour", String.valueOf(disponiveisPorBanda[2]));
+            responseContext.getHeaders().add("X-RateLimit-Limit-Hour", String.valueOf(bandas[2].getCapacity()));
+        }
+    }
+
+    private Bucket criaBalde() {
+        Bandwidth limitePorSegundo = Bandwidth.classic(
+                requestsPorSegundo,
+                Refill.intervally(requestsPorSegundo, Duration.ofSeconds(1)));
+        Bandwidth limitePorMinuto = Bandwidth.classic(
+                requestsPorMinuto,
+                Refill.intervally(requestsPorMinuto, Duration.ofMinutes(1)));
+        Bandwidth limitePorHora = Bandwidth.classic(
+                requestsPorHora,
+                Refill.intervally(requestsPorHora, Duration.ofHours(1)));
 
         return Bucket.builder()
                 .addLimit(limitePorSegundo)
@@ -96,11 +141,13 @@ public class RateLimitFilter implements ContainerRequestFilter, ContainerRespons
         if (instanteDeDesbloqueio == null) return false;
 
         if (System.currentTimeMillis() < instanteDeDesbloqueio) {
-            ipsBloqueados.remove(ip);
-            return false;
+            return true;
         }
 
-        return true;
+        ipsBloqueados.remove(ip);
+        log.info("[Ratelimit] IP: {} desbloqueado (balde recarregado com {} tokens/s)", ip, requestsPorSegundo);
+
+        return false;
     }
 
     private Response respostaToManyRequests(String ip) {
@@ -115,7 +162,8 @@ public class RateLimitFilter implements ContainerRequestFilter, ContainerRespons
                         "Erro", "Limite de requisições excedito",
                         "mensagem", "Aguarde " + minutosBloqueio + " minuto(s) antes de tentar novamente",
                         "bloqueadoAteInstante", bloqueadoAteInstante,
-                        "bloqueadoAteHorario", bloqueadoAteHorario.toString()
+                        "bloqueadoAteHorario", bloqueadoAteHorario.toString(),
+                        "ip", ip
                 )).build();
     }
 
